f4ab7f7fddfebda81607ac4bfabbed5e5bd45917
Constantin Adler <constantin.adler@icloud.com>
2025-05-14 19:00:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Brightroom",
   platforms: [
-    .iOS(.v16)
+    .iOS(.v17)
   ],
   products: [
     .library(name: "BrightroomEngine", targets: ["BrightroomUI"]),
@@ -15,6 +15,7 @@ let package = Package(
     .package(url: "https://github.com/VergeGroup/Verge", from: "14.0.0-beta.7"),
     .package(url: "https://github.com/FluidGroup/TransitionPatch", from: "1.0.3"),
     .package(url: "https://github.com/FluidGroup/PrecisionLevelSlider", from: "2.1.0"),
+    .package(url: "https://github.com/FluidGroup/Brightroom.git", from: "2.2.0")
   ],
   targets: [
     .target(
